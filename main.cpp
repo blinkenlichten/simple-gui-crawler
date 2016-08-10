8b3801f868c9fb25510db0c05a68cc9573d54b59
Bogdan Maslowsky <blinkofnight@gmail.com>
2016-08-10 15:53:19 +0300

    manage qt paths in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include "widget.h"
 #include <QApplication>
 #include "webgrep/crawler.h"
+#include <QDir>
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+  {
+    QDir dir(QApplication::applicationDirPath());
+    QStringList lib_paths = QApplication::libraryPaths();
+    lib_paths << QApplication::applicationDirPath();
+#ifdef __MACH__
+    lib_paths << dir.absolutePath() << dir.absolutePath() + "/plugins";
+    dir.cdUp();
+    lib_paths << dir.absolutePath();
+    dir.cd("PlugIns");
+    lib_paths << dir.absolutePath();
+
+    sett->default_group_values(uri_map, "uri");
+#endif
+    dir.cd("plugins");
+    lib_paths << dir.absolutePath();
+    QApplication::setLibraryPaths(lib_paths);
+  }
   Widget w;
   w.show();
 
